70d09b1226879ea2e28a2ad372726649b60ebfc5
Aj007ar <aj007.ar@gmail.com>
2022-08-29 17:41:55 +0530

    [Ajay] Added: Algorithm Programs

---
+++ AlogorithmPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlogorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">----------------------------> WELCOME TO ALGORITHM PROGRAMS <--------------------------------<");
+        }
+    }
+}
