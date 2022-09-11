5c01d8bc6cd9216367095685d5c215c81e18a8fd
Aj007ar <aj007.ar@gmail.com>
2022-09-11 18:11:33 +0530

    [Ajay] Added: Binary Search

--- AlogorithmPrograms/BinarySearch.cs
+++ AlogorithmPrograms/BinarySearch.cs
@@ -8,35 +8,50 @@ namespace AlogorithmPrograms
 {
     internal class BinarySearch
     {
-        public void binarySearch()
+        public static void Binarysearch()
         {
-            int[] arr = new int[100];
-            Console.WriteLine("Enter number of element in array");
-            string s = Console.ReadLine();
-            int x = Int32.Parse(s);
-            Console.WriteLine("Enter Elements");
-            for(int  i = 0; i < x; i++)
+            //Reads from file
+            string text = File.ReadAllText(@"D:\BL\LinuxBatch-560\LFP-183-DotNet\AlgorithmProgram\AlgotithamPrograms\AlogorithmPrograms\Countries.txt");
+            List<string> list = new List<string>(text.Split(" "));
+            list.Sort();
+            foreach (string element in list)
             {
-                string s1 = Console.ReadLine();
-                arr[i] = Int32.Parse(s1);
+                Console.WriteLine(element);
             }
-            Console.WriteLine("enter search element");
-            string s3 = Console.ReadLine();
-            int x2 = Int32.Parse(s3);
-            int low = 0;
-            int high = x - 1;
-            while (low <= high)
-            {
-                int mid = (low + high) / 2;
-                if (x2 < arr[mid])
-                    high = mid - 1;
-                else if (x2 > arr[mid])
-                    low = mid + 1;
-                else if (x2 == arr[mid])
-                    Console.WriteLine("Element {0} found at location {1}\n",x2,mid+1);
-                return;
-            }
-            Console.WriteLine("search unsuccessful");
+            Console.WriteLine("\n\nEnter the word to be found");
+            string key = Console.ReadLine();
+
+            bool result = Search(list, 0, list.Count - 1, key);
+            if (result == true)
+                Console.WriteLine("Found {0}", key);
+            else
+                Console.WriteLine("Not Found {0}", key);
+
         }
+        //Binary search where elements are compared by finding mid
+        public static bool Search(List<string> list, int start, int end, string key)
+        {
+            bool found = false;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                int res = key.CompareTo(list[mid]);
+                if (res == 0)
+                    found = true;
+                if (res > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+
+
+            return found;
+        }
+
     }
 }
--- AlogorithmPrograms/Program.cs
+++ AlogorithmPrograms/Program.cs
@@ -2,11 +2,25 @@
 {
     internal class Program
     {
+		public void select()
+        {
+            Console.WriteLine("Welcome TO Algorithm Programs");
+            Console.WriteLine("Choose your option");
+            int option=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n1.Binary Search");
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("***Binary Search***");
+                    BinarySearch.Binarysearch();
+                    select();
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(">----------------------------> WELCOME TO ALGORITHM PROGRAMS <--------------------------------<");
-            BinarySearch search = new BinarySearch();
-            search.binarySearch();  
+            Program program = new Program();
+            program.select();
         }
-    }
+	}
 }
